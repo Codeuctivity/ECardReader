3629f142c859b222dff7fd161ae7ca2e8697ff6f
DESKTOP-PNQ0E4F\stefan <stefan.seeland@t-systems.at>
2017-10-12 23:57:36 +0200

    pushing my old ecard reader i wrote 6 years ago without adpu commands and parameter

---
+++ ECardReader/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace ECardReader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBoxLog = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.richTextBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.richTextBoxLog);
+            this.splitContainer1.Size = new System.Drawing.Size(1294, 610);
+            this.splitContainer1.SplitterDistance = 678;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(678, 610);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBoxLog
+            // 
+            this.richTextBoxLog.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBoxLog.Location = new System.Drawing.Point(0, 0);
+            this.richTextBoxLog.Name = "richTextBoxLog";
+            this.richTextBoxLog.Size = new System.Drawing.Size(612, 610);
+            this.richTextBoxLog.TabIndex = 0;
+            this.richTextBoxLog.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1294, 610);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBoxLog;
+    }
+}
+
---
+++ ECardReader/Form1.cs
@@ -0,0 +1,258 @@
+﻿using GemCard;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ECardReader
+{
+    public partial class Form1 : Form
+    {
+        private string reader;
+        private CardBase m_iCard = null;
+        private APDUParam m_apduParam = null;
+        private List<string> readerList = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SelectICard();
+            SetupReaderList();
+        }
+
+        private void SelectICard()
+        {
+            try
+            {
+                if (m_iCard != null)
+                    m_iCard.Disconnect(DISCONNECT.Unpower);
+
+                m_iCard = new CardNative();
+                richTextBoxLog.Text += "CardNative implementation used";
+
+                m_iCard.OnCardInserted += new CardInsertedEventHandler(m_iCard_OnCardInserted);
+                m_iCard.OnCardRemoved += new CardRemovedEventHandler(m_iCard_OnCardRemoved);
+            }
+            catch (Exception ex)
+            {
+                richTextBoxLog.Text += ex.Message;
+            }
+        }
+
+        private void SetupReaderList()
+        {
+            try
+            {
+                string[] sListReaders = m_iCard.ListReaders();
+
+                if (sListReaders != null)
+                {
+                    for (int nI = 0; nI < sListReaders.Length; nI++)
+                    {
+                        readerList.Add(sListReaders[nI]);
+                        log("Found" + sListReaders[nI]);
+                    }
+                    log("Selected " + sListReaders[0]);
+                    //// Start waiting for a card
+                    reader = sListReaders[0];
+                    m_iCard.StartCardEvents(reader);
+                    log("Waiting for a card");
+                }
+                else
+                {
+                    log("Found no smartcardreaders");
+                }
+            }
+            catch (Exception ex)
+            {
+                richTextBoxLog.Text += ex.Message;
+            }
+        }
+
+        private delegate void logDelegate(string text);
+
+        private void log(string p)
+        {
+            richTextBoxLog.Text += p + "\n";
+        }
+
+        /// <summary>
+        /// CardInsertedEventHandler
+        /// </summary>
+        private void m_iCard_OnCardInserted(string reader)
+        {
+            richTextBoxLog.Invoke(new logDelegate(log), new object[] { "Card inserted" });
+            try
+            {
+                var hexInfo = Bytes2String(invokeSmartcard());
+                richTextBoxLog.Invoke(new logDelegate(log), new object[] { hexInfo });
+                richTextBoxLog.Invoke(new logDelegate(log), new object[] { parse(hexInfo) });
+            }
+            catch (SmartCardException exSC)
+            {
+                richTextBoxLog.Invoke(new logDelegate(log), new object[] { exSC.Message });
+            }
+            catch (Exception ex)
+            {
+                richTextBoxLog.Invoke(new logDelegate(log), new object[] { ex.Message });
+            }
+        }
+
+        /// <summary>
+        /// CardRemovedEventHandler
+        /// </summary>
+        private void m_iCard_OnCardRemoved(string reader)
+        {
+            richTextBoxLog.Invoke(new logDelegate(log), new object[] { "Card removed" });
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            {
+                try
+                {
+                    m_iCard.Disconnect(DISCONNECT.Unpower);
+                    m_iCard.StopCardEvents();
+                }
+                catch
+                {
+                }
+            }
+        }
+
+        private APDUParam BuildParam(string P1, string P2, string Le)
+        {
+            byte bP1 = byte.Parse(P1, NumberStyles.AllowHexSpecifier);
+            byte bP2 = byte.Parse(P2, NumberStyles.AllowHexSpecifier);
+            byte bLe = byte.Parse(Le);
+
+            APDUParam apduParam = new APDUParam();
+            apduParam.P1 = bP1;
+            apduParam.P2 = bP2;
+            apduParam.Le = bLe;
+
+            // Update Current param
+            m_apduParam = apduParam.Clone();
+
+            return apduParam;
+        }
+
+        private byte[] invokeSmartcard()
+        {
+            //i blanked the commands, and parameters - i am not sure if i am allowed to publish them
+            APDUCommand apduEcard1 = new APDUCommand(0x00, 0x00, 04, 0x00, null, 0);
+            APDUCommand apduEcard2 = new APDUCommand(0x00, 0x00, 0, 0, null, 0);
+            APDUCommand apduEcard3 = new APDUCommand(0x00, 0x00, 00, 0x00, null, 255);
+            APDUResponse apduResp;
+
+            CardNative iCard = new CardNative();
+            iCard.Connect(reader, SHARE.Shared, PROTOCOL.T0orT1); //connected
+            // Ecard1
+            byte[] data1 = new byte[] { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+            APDUParam apduParam = new APDUParam();
+            apduParam.Data = data1;
+            apduEcard1.Update(apduParam);
+            apduResp = iCard.Transmit(apduEcard1);
+            // Ecard2
+            apduParam.Data = new byte[] { 0x00, 0x00 };
+            apduEcard2 = new APDUCommand(0x00, 0x00, 00, 0x0C, apduParam.Data, 0);
+            apduEcard2.Update(apduParam);
+            apduResp = iCard.Transmit(apduEcard2);
+            // Ecard3
+            apduParam.Data = new byte[] { 0 };
+            apduEcard3 = new APDUCommand(0x00, 0x00, 00, 0x00, null, 255);
+            apduResp = iCard.Transmit(apduEcard3);
+            return apduResp.Data;
+        }
+
+        private string convertHexToAscii(string info)
+        {
+            //http://msdn.microsoft.com/en-us/library/bb311038.aspx
+            string hexValues = info;
+            string[] hexValuesSplit = hexValues.Trim().Split(' ');
+            string returner = string.Empty;
+            try
+            {
+                foreach (string hex in hexValuesSplit)
+                {
+                    if (hex != "  ")
+                    {
+                        // Convert the number expressed in base-16 to an integer.
+                        int value = Convert.ToInt32(hex, 16);
+                        // Get the character corresponding to the integral value.
+                        string stringValue = Char.ConvertFromUtf32(value);
+                        char charValue = (char)value;
+                        //hex, value, stringValue, charValue);
+                        returner += charValue;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            string returner1 = string.Empty;
+            foreach (var item in returner.Split(new char[] { '\b', '\n', '\r', '\0', '\f' }))
+            {
+                returner1 += item + "\n";
+            }
+            return returner1;
+        }
+
+        private static string DecodeBytes2String(byte[] bytes)
+        {
+            return System.Text.Encoding.GetEncoding(1252).GetString(bytes);
+        }
+
+        private static string Bytes2String(byte[] data)
+        {
+            StringBuilder sDataOut;
+
+            if (data != null)
+            {
+                sDataOut = new StringBuilder(data.Length * 2);
+                for (int nI = 0; nI < data.Length; nI++)
+                    sDataOut.AppendFormat("{0:X02} ", data[nI]);
+            }
+            else
+                sDataOut = new StringBuilder();
+
+            return sDataOut.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBoxLog.Text = parse(richTextBox1.Text);
+        }
+
+        private string parse(string p)
+        {
+            var indexSvnr = p.IndexOf(Properties.Settings.Default.preSvnr, 0) + Properties.Settings.Default.preSvnr.Length + 1;
+            //von ende des prefix 30 zeichen (svnr ist immer gleich lang)
+            string svnr = p.Substring(indexSvnr, 30);
+
+            var indexFirstName = p.IndexOf(Properties.Settings.Default.preFirstName, indexSvnr) + Properties.Settings.Default.preFirstName.Length + 8;
+            //von ende des bekannten prefix +8 unbekannte zeichen bis 6 zeichen vor dem bekannten prefixteil von prelastname
+            int firstNameLength = p.IndexOf(Properties.Settings.Default.preLastName, indexSvnr) - 6 - indexFirstName;
+            string firstName = p.Substring(indexFirstName, firstNameLength);
+
+            var indexPreLastName = p.IndexOf(Properties.Settings.Default.preLastName, indexFirstName) + Properties.Settings.Default.preLastName.Length + 10;
+            int lastnameLength = p.IndexOf(Properties.Settings.Default.preBirthdate, indexPreLastName) - indexPreLastName;
+            string lastName = p.Substring(indexPreLastName, lastnameLength);
+
+            var indexBirthdate = p.IndexOf(Properties.Settings.Default.preBirthdate, indexPreLastName) + Properties.Settings.Default.preBirthdate.Length + 1;
+            string Brithdate = p.Substring(indexBirthdate, 24);
+
+            var indexSex = p.IndexOf(Properties.Settings.Default.preSex, indexBirthdate) + Properties.Settings.Default.preSex.Length + 1;
+            //immer 2 zeichen lang
+            string Sex = p.Substring(indexSex, 3);
+
+            return convertHexToAscii(svnr) + "\n" + convertHexToAscii(firstName) + "\n" + convertHexToAscii(lastName) + "\n" + convertHexToAscii(Brithdate) + "\n" + convertHexToAscii(Sex) + "\n";
+        }
+    }
+}
